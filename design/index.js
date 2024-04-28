7c6c3eb0ba8ce2b96dd13995262d2b183b883cc1
CarlosVeGo <carlos.velezg2011@gmail.com>
2024-04-28 12:04:37 -0500

    buscador de banderas api completado

---
+++ design/index.js
@@ -0,0 +1,108 @@
+const main = document.querySelector("main");
+const continentes = document.querySelector("#continentes");
+const apiUrl = `https://restcountries.com/v3.1/all`;
+const searcher = document.querySelector(".searcher");
+
+function filter(){ 
+    
+    fetch(apiUrl).then((data) => data.json().then((res)=>{
+        
+        res.forEach(re => {
+            const name = re.name.common
+            const poblacion = re.population
+            const capital = re.capital
+            const image = re.flags.png
+            const region = re.region
+            
+            main.innerHTML +=`
+            <div class="country" id="${name}">
+                <img src="${image}">
+
+                <div class="info">
+                    <p class="title-name">${name}</p>
+                    <p> <span class="title"> Population: </span>${poblacion}</p>
+                    <p><span  class="title">Region: </span>${region} </p>
+                    <p><span class="title">Capital: </span>${capital}</p>
+                </div>
+            </div>
+            `
+        });
+
+    })).catch((err)=> main.innerHTML = `Try Again Later`)
+}
+
+continentes.addEventListener("change", () => {
+    
+    const continente = continentes.value;
+    
+    main.innerHTML = `Buscando Banderas`;
+
+    if(continente === "filter"){
+        filter()
+    }else{
+            fetch(apiUrl).then((data) => data.json().then((res)=>{
+            res.forEach( re => {
+
+                const name = re.name.common
+                const region = re.region
+                const poblacion = re.population
+                const capital = re.capital
+                const image = re.flags.png
+
+                if(continente === region){
+                    main.innerHTML +=`
+                    <div class="country" id="${name}">
+                        <img src="${image}">
+                        <div class="info">
+                            <p class="title-name">${name}</p>
+                            <p> <span class="title"> Population: </span>${poblacion}</p>
+                            <p><span  class="title">Region: </span>${region} </p>
+                            <p><span class="title">Capital: </span>${capital}</p>
+                        </div>
+                    </div>
+                    `
+                }
+            })
+        })).catch((err)=> main.innerHTML = `Try Again Later`)
+    }
+})
+
+searcher.addEventListener("input", () => {
+    
+    const input = searcher.value.toLowerCase();
+    main.innerHTML =`${input}`
+    console.log(input)
+    const long = input.length
+    
+    if( input.length === 0 ){
+        filter()
+    }else{
+        main.innerHTML =``
+        fetch(apiUrl).then((data) => data.json().then((res)=>{
+            res.forEach( re => {
+                
+                const name = re.name.common
+                const region = re.region
+                const poblacion = re.population
+                const capital = re.capital
+                const image = re.flags.png 
+
+                if(name.substr(0,long).toLowerCase()===input){
+                    main.innerHTML +=`
+                    <div class="country" id="${name}">
+                        <img src="${image}">
+                        <div class="info">
+                            <p class="title-name">${name}</p>
+                            <p> <span class="title"> Population: </span>${poblacion}</p>
+                            <p><span  class="title">Region: </span>${region} </p>
+                            <p><span class="title">Capital: </span>${capital}</p>
+                        </div>
+                    </div>
+                    `
+                }
+            })
+        })).catch((err)=> main.innerHTML = `Try Again Later`)
+    }
+})
+
+filter()
